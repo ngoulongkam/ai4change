9d914cb878d62190d864c411d67517fbf68c58a0
ngoulongkam <ngoulongkam2015@gmail.com>
2018-09-20 21:01:55 +0100

    moving auth into its own file

---
+++ auth.py
@@ -0,0 +1,5 @@
+import tweepy
+import twitter_credentials
+
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
--- twitter_streamer.py
+++ twitter_streamer.py
@@ -1,10 +1,7 @@
 import tweepy
-import twitter_credentials
+import auth
 
-auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth)
+api = tweepy.API(auth.auth)
 
 public_tweets = api.home_timeline()
 for tweet in public_tweets:

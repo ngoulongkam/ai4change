fa5346f9096ccabbef04001863a9d710d674c8c8
Luke Smith <luke.smith@digital.hmrc.gov.uk>
2018-09-28 08:37:19 +0100

    progress check in

--- application.py
+++ application.py
@@ -4,8 +4,11 @@ import tweepy
 import auth
 from monkeylearn import MonkeyLearn
 import monkeylearn_credentials
+import json
 
 application = Flask(__name__)
+ml = MonkeyLearn(monkeylearn_credentials.MONKEY_AUTH_TOKEN)
+model_id = 'cl_pi3C7JiL'
 
 
 @application.route("/latest", methods=["GET"])
@@ -13,12 +16,27 @@ def get_latest_messages():
     api = tweepy.API(auth.auth)
 
     public_tweets = api.home_timeline()
+    # for tweet in public_tweets:
+    #     data = [tweet.text]
+    #     result = ml.classifiers.classify(model_id, data)
+    #     # Data needed
+    #     # sentimentData[] - created_at (tweet), confidence(ml), tag_name(ml), text(tweet,ml)
+    #     # confidenceLevel - averagePositiveConfidence(calc)
+    #     # averageNeutralConfidence(calc), averageNegativeConfidence(calc), time_zone(tweet)
+    response = build_response(public_tweets)
+    return jsonify(response), 200
+
+
+def build_response(public_tweets):
+    sentiment_data = {}
     for tweet in public_tweets:
-        ml = MonkeyLearn(monkeylearn_credentials.MONKEY_AUTH_TOKEN)
         data = [tweet.text]
-        model_id = 'cl_pi3C7JiL'
-        result = ml.classifiers.classify(model_id, data)
-        return jsonify(result.body), 200
+        ml_result = json.loads(ml.classifiers.classify(model_id, data))
+        sentiment_data.update({"created_at": tweet.created_at})
+        sentiment_data.update({"confidence": ml_result.body['classifications']['confidence']})
+        sentiment_data.update({"tag_name": ml_result.body['classifications']['tag_name']})
+        sentiment_data.update({"text": tweet.text})
+    return jsonify(sentiment_data)
 
 
 if __name__ == "__main__":
